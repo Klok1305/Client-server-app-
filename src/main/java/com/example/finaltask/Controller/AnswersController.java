e543cc8dc7076b49b1bc5e76580a0386f6b52682
Klok <klok1305@gmail.com>
2022-01-04 22:39:58 +0300

    all

--- src/main/java/com/example/finaltask/Controller/AnswersController.java
+++ src/main/java/com/example/finaltask/Controller/AnswersController.java
@@ -2,57 +2,34 @@ package com.example.finaltask.Controller;
 
 import com.example.finaltask.Models.Parser;
 import com.example.finaltask.Models.Transactions;
+import com.example.finaltask.repo.TransactionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 @Controller
 public class AnswersController {
     @GetMapping("/answers")
-    public String getAnswers(){
-        return "answers_page";
-    }
-
-
-    public List<Double> answerish(double id){
-        List<Long> ClinetIDs;
-        List<Double> Ammounts;
-        List<Double> needed = new ArrayList<>();
+    public String getAnswers() throws IOException {
         Parser p = new Parser();
-        ClinetIDs = p.getClients();
-        Ammounts = p.getAmounts();
-        for (int i= 0; i < ClinetIDs.size(); i++){
-            if (ClinetIDs.get(i) == id){
-                needed.add(Ammounts.get(i));
-            }
-        }
-        double max = Collections.max(needed);
-        int max_n = 0;
-        double num = 0;
-        for (int i= 0; i < needed.size(); i++){
-            needed.set(i, Math.abs(needed.get(i)));
-            if (Collections.frequency(needed, needed.get(i)) > max_n){
-                max_n = Collections.frequency(needed, needed.get(i));
-                num = needed.get(i);
-            }
-        }
-        List<Double> out = new ArrayList<>();
-        out.add(max);
-        out.add(num);
-        out.add(Double.valueOf(max_n));
-        System.out.println(needed);
-        return needed;
-    }
-    @PostMapping
-    public String getTransactionsPage(Model model){
-        List<Double> nums = new ArrayList<Double>();
-        nums = answerish(606275);
-        model.addAttribute("nums", nums);
+        p.UseScanner();
+        System.out.println(p.answerish(606275).toString());
         return "answers_page";
+    }
+
+    @PostMapping
+    public String getTransactionsPage(Model model) throws IOException {
+
+        return null;
     }
 }
--- src/main/java/com/example/finaltask/Models/Parser.java
+++ src/main/java/com/example/finaltask/Models/Parser.java
@@ -4,8 +4,10 @@ import com.example.finaltask.repo.TransactionsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -19,8 +21,7 @@ public class Parser {
         BufferedReader reader = new BufferedReader(new FileReader(file_path));
         String line = null;
         Scanner scanner = null;
-        int index = 0;
-
+        int flag = 0;
         Transactions tran = new Transactions();
 
 
@@ -29,7 +30,7 @@ public class Parser {
             scanner.useDelimiter(",");
             while (scanner.hasNext()) {
                 String data = scanner.next();
-                switch (index) {
+                switch (flag) {
                     case 0:
                         tran.setCustomer_id(Long.valueOf(data));
                         break;
@@ -49,24 +50,46 @@ public class Parser {
                         tran.setTerm_id(Double.parseDouble(data));
                         break;
                     default:
-                        System.out.println("Некорректные данные::" + data);
+                        System.out.println("Некорректные данные:" + data);
                         break;
                                 }
-                index++;
+                flag++;
             }
-            index = 0;
+            flag = 0;
             ClientIDs.add(tran.getCustomer_id());
             Amounts.add(tran.getAmount());
             //tranrep.save(tran);
         }
-
+        List<Double> n;
+        n = answerish(606275);
         reader.close();
+        FileWriter a = new FileWriter("C:\\Users\\B.N.B\\IdeaProjects\\demo\\temp.txt");
+        a.write(n.get(0).toString() + ',' + n.get(1).toString()+','+ n.get(2).toString()+','+n.get(3).toString());
+        a.close();
     }
-    public List<Long> getClients(){
-        return ClientIDs;
-    }
-
-    public List<Double> getAmounts(){
-        return Amounts;
+    public List<Double> answerish(double id) {
+        List<Double> needed = new ArrayList<>();
+        Parser p = new Parser();
+        for (int i = 0; i < ClientIDs.size(); i++) {
+            if (ClientIDs.get(i) == id) {
+                needed.add(Amounts.get(i));
+            }
+        }
+        List<Double> out = new ArrayList<>();
+        double max = Collections.max(needed);
+        int max_n = 0;
+        double num = 0;
+        for (int i = 0; i < needed.size(); i++) {
+            needed.set(i, Math.abs(needed.get(i)));
+            if (Collections.frequency(needed, needed.get(i)) > max_n) {
+                max_n = Collections.frequency(needed, needed.get(i));
+                num = needed.get(i);
+            }
+        }
+        out.add(606275.0);
+        out.add(max);
+        out.add(num);
+        out.add(Double.valueOf(max_n));
+        return out;
     }
 }
